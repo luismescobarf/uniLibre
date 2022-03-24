6dbf7370e4bf13fcd0951682cc0e1d177aa98d68
luismescobarf <luismescobarf@gmail.com>
2022-03-24 00:44:13 -0500

    Script para asignación del tipo de Quiz

--- LA/scriptAsignacionQuiz.py
+++ LA/scriptAsignacionQuiz.py
@@ -1,65 +1,76 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#Librerías
+import pprint as pp
+import random
+
+#Códigos de los estudiantes del grupo
+codigosEstudiantes = """
+1114149109
+1085716618
+1089931822
+1088244874
+1089599453
+1004720345
+1004718114
+1004776323
+1089932201
+1077997121
+1088240040
+1108332514
+1114148533
+1111662143
+1057330367
+1004719303
+1089598522
+1088241078
+1029980139
+1004520854
+1000179645
+1093589016
+1114398740
+1088355585
 """
-Created on Mon Nov  9 22:22:07 2020
+#Limpiar comienzo y final
+codigosEstudiantes = codigosEstudiantes.strip()
 
-@author: luismiguelescobarfalcon
-"""
+#Convertir el bloque de códigos en un listado
+listadoCodigos = codigosEstudiantes.split("\n")
 
-#Impresión incremental
-def cadenaDecremental(cadena):
-    decremento_general = 0
-    for i in range(len(cadena)+1 - decremento_general):
-        incremento_palabra = 0
-        for j in range(len(cadena)+1 - decremento_general):                        
-            for k in range(incremento_palabra):
-                print(cadena[k],end='')
-            print(' ',end='')
-            incremento_palabra += 1
-        print()
-        decremento_general += 1
+#Mostrar el contenido almacenado en el contenedor
+#print("Códigos Ingresados:")
+#[print(codigo) for codigo in listadoCodigos]
 
-#Impresión decremental
-def cadenaIncremental(cadena):
-    limitador_palabras = len(cadena)    
-    for i in range(len(cadena)+1):
-        limitador_letras = 0               
-        for j in range(len(cadena)+1 - limitador_palabras):             
-            for k in range(limitador_letras):
-                print(cadena[k],end='')
-            limitador_letras += 1
-            print(' ',end='')
-        print()
-        limitador_palabras -= 1   
-    
+#Opciones de asignación
+opcionesAsignacion = ["Quiz Tipo A", "Quiz Tipo B"]
 
-cadena = 'carro'
-cadenaDecremental(cadena)
-print()
-cadenaIncremental(cadena)
+#Contenedor de asignaciones
+detalleAsignaciones = list()
 
+#Preparar conjuntos de control
+estudiantesSinAsignar = set(listadoCodigos)
+estudiantesAsignados = set()
 
-palabra=str(input('Ingrese la palabra: '))
-x=0
-letra=1
+#Asignar la primera opción a la mitad del listado de estudiantes seleccionados aleatoriamente
+numeroMitadGrupo = len(listadoCodigos) // 2
+for _ in range(numeroMitadGrupo):	
+	while True:
+		elegido = random.choice(listadoCodigos)
+		if elegido not in estudiantesAsignados:
+			estudiantesAsignados.add(elegido)
+			estudiantesSinAsignar.remove(elegido)
+			detalleAsignaciones.append([elegido,opcionesAsignacion[0]])
+			break
 
-while x<len(palabra):
-    print(palabra[0:letra],end="")
-    letra=letra+1
-    x=x+1
-
-cadena1 = 'hola'
-cadena2 = 'mundo'
-print(cadena1,end='')
-print(' ',cadena2,end='')
+#Asignar la segunda opción al resto
+listadoRestantes = list(estudiantesSinAsignar)
+for estudianteFaltante in listadoRestantes:
+	detalleAsignaciones.append([estudianteFaltante,opcionesAsignacion[1]])
+	
+#Presentar toda la asignación
+print("------------------------------")
+print("CódigoEstudiante -> Tipo Quiz")
+print("------------------------------")
+[print(f"{asignacion[0]} -> {asignacion[1]}") for asignacion in detalleAsignaciones]
 
 
 
 
-    
-    
-
-            
-            
-            
-        
