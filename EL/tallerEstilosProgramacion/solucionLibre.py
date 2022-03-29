69355f68cd3b0d75e0356e2bf2e0e3a925d26f4d
luismescobarf <luismescobarf@gmail.com>
2022-03-29 14:52:17 -0500

    Primera solución libre del requerimiento

---
+++ EL/tallerEstilosProgramacion/solucionLibre.py
@@ -0,0 +1,27 @@
+#Variables de entrada
+codigo = "AA0010276"
+nota1 = 40
+nota2 = 50
+nota3 = 39
+nota4 = 76
+nota5 = 96
+
+#Coleccionar las notas
+listaNotas = [nota1, nota2, nota3, nota4, nota5]
+
+#Identificar el menor
+notaMenor = 99999
+posicion = int()
+for i in range(len(listaNotas)):
+    if listaNotas[i] < notaMenor:
+        notaMenor = listaNotas[i]
+        posicion = i
+        
+#Calcular el promedio ajustado en escala de 0 a 100
+promedio100 = (sum(listaNotas) - notaMenor)/4
+
+#Cambio de escala
+promedio5 = round( promedio100/20  ,2)
+
+#Presentar el resultado con el mensaje establecido
+print(f"El promedio ajustado del estudiante {codigo} es: {promedio5} ")
