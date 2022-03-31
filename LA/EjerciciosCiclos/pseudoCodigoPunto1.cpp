6244e5e8cb7ccd26df0db302e1c3bc131afa65f3
luismescobarf <luismescobarf@gmail.com>
2022-03-31 09:38:28 -0500

    Carpeta solución Ejercicios Ciclos LA

---
+++ LA/EjerciciosCiclos/pseudoCodigoPunto1.cpp
@@ -0,0 +1,10 @@
+/*
+Desarrollar un algoritmo que permita ingresar 
+al usuario tantos números como quiera. 
+Cuando el usuario decida terminar de ingresar los números, 
+reportar cuántos números fueron ingresados en total, 
+luego cuántos números pares, cuántos números impares y 
+el promedio de los valores ingresados. 
+Solamente incluir valores positivos en el promedio, 
+aunque el usuario podría ingresar valores negativos.
+ */ 
---
+++ LA/EjerciciosCiclos/pseudoCodigoPunto2.cpp
@@ -0,0 +1,8 @@
+/*
+Diseñar un algoritmo que realice totalice por separado 
+los números pares y los números impares comprendidos 
+entre 0 y un valor ingresado por el usuario. 
+El valor ingresado puede ser positivo o negativo, 
+por lo tanto, el algoritmo debe realizar la sumatoria 
+en ambas direcciones (sumatoria algebráica).
+*/ 
