27d98af92e7f8a7b143ed02cf8a447903b9e1631
luismescobarf <luismescobarf@gmail.com>
2022-04-07 18:26:40 -0500

    Cierre clase de Lógica, inicio caso de estudio BD

---
+++ BD/casoProduccionAguacate/TallerSegundoCorteRequerimientos.sql
@@ -0,0 +1,33 @@
+-----------------------
+-- Taller Segundo Corte
+-----------------------
+-- 1. ¿Cuáles son las 3 enfermedades más comunes que presentan los árboles?
+-- 2. ¿Cuál es el árbol que más se ha enfermado en la producción?
+-- 3. ¿Cuál es el árbol que menos se ha fumigado?
+-- 4. ¿Quién es el trabajador que tiene el mayor número de recolectas?
+-- 5. Listado de los árboles rankeados por su producción de mayor a menor (peso de las recolectas que le han realizado).
+-- 6. Las producciones rankeadas de mayor a menor por la proporción entre el peso acumulado de esta y el número de recolectas asociadas.
+-- 7. ¿De qué calidad de aguacates se ha producido más?
+-- 8. Detalle de cuánto ha producido cada árbol de cada calidad, mostrando de qué calidad produjo más y de qué calidad produjo menos.
+-- 9. Los 4 meses del año donde más enfermedades se diagnostican.
+-- 10. Los 10 árboles menos productivos que más se han enfermado.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LA/introPython/sumarDosNumeros.py
@@ -0,0 +1,19 @@
+#Este algoritmo recibe dos números,
+#los suma y presenta el resultado
+"""
+Este es un comentario
+de
+varias líneas
+"""
+#Asignación por código
+#a = 12 
+
+#Entrada por teclado
+a = int(input('Ingrese el primer número: '))
+b = int(input('Ingrese el segundo número: '))
+
+#Cómputo
+c = a + b
+
+#Salida por pantalla
+print(f"La suma de {a} y {b} es: {c}")
