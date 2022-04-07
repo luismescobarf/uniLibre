99f46576890b246e2a8c4c97dba98805c3532f63
luismescobarf <luismescobarf@gmail.com>
2022-04-07 09:15:56 -0500

    Inicio ejercicios cadenas en clase

---
+++ LA/EjerciciosCadenasClase/pseudoCodigoPunto1.cpp
@@ -0,0 +1,15 @@
+/*
+Plantear e implementar un algoritmo que obtenga el número
+de vocales que tiene una palabra (ingresada por el usuario)
+ */ 
+Algoritmo NumeroVocales
+Variables:	
+Inicio
+	
+Fin
+
+
+
+
+
+
