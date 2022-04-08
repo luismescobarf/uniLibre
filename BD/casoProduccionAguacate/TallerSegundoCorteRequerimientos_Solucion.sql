1860f5dbda7927fff1046f96be6f2f6ffe4d99eb
luismescobarf <luismescobarf@gmail.com>
2022-04-07 19:40:40 -0500

    Corrección output primer requerimiento

--- BD/casoProduccionAguacate/TallerSegundoCorteRequerimientos_Solucion.sql
+++ BD/casoProduccionAguacate/TallerSegundoCorteRequerimientos_Solucion.sql
@@ -4,14 +4,25 @@
 -- 1. ¿Cuáles son las 3 enfermedades más comunes que presentan los árboles?
 
 SELECT 
+    e.Nombre,
+    COUNT(*) as Numero_Arboles_Infectados
 FROM Enfermedad e
 JOIN Diagnostico d on e.ID_Enfermedad = d.ID_Enfermedad
-
-
-
-
+GROUP BY d.ID_Enfermedad
+ORDER BY  Numero_Arboles_Infectados DESC
+LIMIT 3;
 
 -- 2. ¿Cuál es el árbol que más se ha enfermado en la producción?
+SELECT
+    d.ID_Arbol,
+    COUNT(*) Numero_Infecciones
+FROM Diagnostico d
+GROUP
+
+
+
+
+
 -- 3. ¿Cuál es el árbol que menos se ha fumigado?
 -- 4. ¿Quién es el trabajador que tiene el mayor número de recolectas?
 -- 5. Listado de los árboles rankeados por su producción de mayor a menor (peso de las recolectas que le han realizado).
