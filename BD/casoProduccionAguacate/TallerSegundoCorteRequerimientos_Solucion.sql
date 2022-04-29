43e65ec707306d1f03b63928e5e94514c8d43dc4
luismescobarf <luismescobarf@gmail.com>
2022-04-29 18:04:04 -0500

    Actualización de las consultas desarrolladas en clase

--- BD/casoProduccionAguacate/TallerSegundoCorteRequerimientos_Solucion.sql
+++ BD/casoProduccionAguacate/TallerSegundoCorteRequerimientos_Solucion.sql
@@ -13,18 +13,52 @@ ORDER BY  Numero_Arboles_Infectados DESC
 LIMIT 3;
 
 -- 2. ¿Cuál es el árbol que más se ha enfermado en la producción?
+
 SELECT
     d.ID_Arbol,
-    COUNT(*) Numero_Infecciones
+    COUNT(*) as Numero_Infecciones
 FROM Diagnostico d
-GROUP
-
-
-
-
+GROUP BY d.ID_Arbol
+ORDER BY Numero_Infecciones DESC
+LIMIT 1;
 
 -- 3. ¿Cuál es el árbol que menos se ha fumigado?
+
+SELECT 
+    f.ID_Arbol,
+    COUNT(*) NumeroFumigaciones
+FROM Fumiga f
+GROUP BY f.ID_Arbol
+ORDER BY NumeroFumigaciones
+LIMIT 1;
+
+SELECT 
+    f.ID_Arbol,
+    COUNT(*) NumeroFumigaciones
+FROM Fumiga f
+GROUP BY f.ID_Arbol
+HAVING NumeroFumigaciones IN (
+    SELECT         
+        COUNT(*) NumeroFumigaciones
+    FROM Fumiga f
+    GROUP BY f.ID_Arbol    
+    ORDER BY NumeroFumigaciones
+    LIMIT 1
+)
+ORDER BY NumeroFumigaciones;
+
 -- 4. ¿Quién es el trabajador que tiene el mayor número de recolectas?
+
+SELECT 
+    t.ID_Trabajador,
+    t.Nombre,
+    COUNT(*) as NumeroRecolectas
+FROM Trabajador t
+JOIN Recolecta r ON t.ID_Trabajador = r.ID_Trabajador
+GROUP BY r.ID_Trabajador
+ORDER BY NumeroRecolectas DESC
+LIMIT 1;
+
 -- 5. Listado de los árboles rankeados por su producción de mayor a menor (peso de las recolectas que le han realizado).
 -- 6. Las producciones rankeadas de mayor a menor por la proporción entre el peso acumulado de esta y el número de recolectas asociadas.
 -- 7. ¿De qué calidad de aguacates se ha producido más?
