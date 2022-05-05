c9ee8603f5a4c3ff886b903680d81c1f2ad12b8a
luismescobarf <luismescobarf@gmail.com>
2022-05-05 08:51:01 -0500

    Ejemplos subíndices strings

--- LA/scriptsCadenas/introCadenas.py
+++ LA/scriptsCadenas/introCadenas.py
@@ -22,6 +22,7 @@ print(f"Primera letra: {palabra[0]}")
 
 #Imprimir última letra
 print(f"Última letra: {palabra[ len(palabra)-1 ]}")
+print(f"Última letra (subíndice negativo): {palabra[ -1 ]}")
 
 #Cantidad de elementos
 print(f"Cantidad de elementos: {len(palabra)}")
@@ -29,3 +30,21 @@ print(f"Cantidad de elementos: {len(palabra)}")
 #Cada letra en un renglón
 for i in range( len(palabra) ):
     print(f"Subíndice {i} letra: {palabra[i]}")
+    
+#Recorrer todos los elementos sin subíndice
+print("Recorrido por colección:")
+for letra in palabra:
+    print(letra)
+    
+#Imprimir las primeras tres letras de una palabra recibida
+print("Primera tres letras de la cadena: ")
+for i in range (3):
+    print(palabra[i])
+    
+#Imprimir las primeras tres letras con slice
+print("Emplando slice: ")
+print( palabra[:3] )
+
+#Slice especificando
+pedazoPalabra = palabra[3:5]
+print(f"Pedazo obtenido: {pedazoPalabra}")
