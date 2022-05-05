716af7d8082e300925a81cd059fdc8e4dacd821a
luismescobarf <luismescobarf@gmail.com>
2022-05-05 08:31:39 -0500

    Primer ejemplo cadenas

--- LA/scriptsCadenas/introCadenas.py
+++ LA/scriptsCadenas/introCadenas.py
@@ -1,1 +1,31 @@
-######
+# Comentario de una línea
+"""
+Esto
+es 
+un
+comentario
+varias 
+líneas
+"""
+
+""""
+Ejemplo: Algoritmo que reciba una cadena
+e imprima la primera letra, la última letra
+y luego cada letra en un renglón separado
+"""
+
+#Recibir cadena
+palabra = input('Ingrese una palabra: ')
+
+#Imprimir primera letra
+print(f"Primera letra: {palabra[0]}")
+
+#Imprimir última letra
+print(f"Última letra: {palabra[ len(palabra)-1 ]}")
+
+#Cantidad de elementos
+print(f"Cantidad de elementos: {len(palabra)}")
+
+#Cada letra en un renglón
+for i in range( len(palabra) ):
+    print(f"Subíndice {i} letra: {palabra[i]}")
