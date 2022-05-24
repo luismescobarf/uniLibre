20e8c89e6c57ea834675dd4b98a2f56ee6d1db1f
luismescobarf <luismescobarf@gmail.com>
2022-05-24 10:37:23 -0500

    Archivos fuente caso institución educativa

---
+++ EL/SalasComputo/casoSalasComputo.py
@@ -0,0 +1,37 @@
+institucion = {
+   'nombreInstitucion':'Institución Educativa Menor',
+   'direccion':'Vereda Campo Grande',
+   'salas':[
+       {
+        'Nombre':'Sala1',
+        'Equipos': [
+            {'CodEquipo':'rc1234','Procesador':'i5','AnioCompra':2016,'ValorCompra':2500000,'Activo':'Si'},
+            {'CodEquipo':'asd56','Procesador':'i7','AnioCompra':2019,'ValorCompra':3000000,'Activo':'Si'},
+            {'CodEquipo':'rc532','Procesador':'i7','AnioCompra':2019,'ValorCompra':3000000,'Activo':'Si'},
+            {'CodEquipo':'asda3','Procesador':'amd','AnioCompra':2014,'ValorCompra':1200000,'Activo':'No'},
+            {'CodEquipo':'asda5','Procesador':'amd','AnioCompra':2014,'ValorCompra':1200000,'Activo':'Si'},
+            {'CodEquipo':'tg151','Procesador':'intelDualCore','AnioCompra':2011,'ValorCompra':1100000,'Activo':'Si'}
+        ]                      
+       },
+       {
+        'Nombre':'Sala Biblioteca',
+        'Equipos': [
+            {'CodEquipo':'tg12','Procesador':'intelDualCore','AnioCompra':2010,'ValorCompra':1350000,'Activo':'Si'},
+            {'CodEquipo':'tg13','Procesador':'intelDualCore','AnioCompra':2010,'ValorCompra':1350000,'Activo':'No'},
+            {'CodEquipo':'tg14','Procesador':'intelDualCore','AnioCompra':2010,'ValorCompra':1350000,'Activo':'Si'},
+            {'CodEquipo':'tg15','Procesador':'intelDualCore','AnioCompra':2011,'ValorCompra':1100000,'Activo':'Si'},
+            {'CodEquipo':'tg16','Procesador':'intelDualCore','AnioCompra':2011,'ValorCompra':1100000,'Activo':'No'},
+            {'CodEquipo':'bib123','Procesador':'i7','AnioCompra':2019,'ValorCompra':3000000,'Activo':'Si'}            
+        ]                      
+       },
+       {
+        'Nombre':'Sala Sistemas',
+        'Equipos': [
+            {'CodEquipo':'sist1','Procesador':'i9','AnioCompra':2021,'ValorCompra':5000000,'Activo':'Si'},
+            {'CodEquipo':'sist2','Procesador':'i9','AnioCompra':2021,'ValorCompra':5000000,'Activo':'Si'},
+            {'CodEquipo':'sist3','Procesador':'i9','AnioCompra':2021,'ValorCompra':5000000,'Activo':'No'},            
+            {'CodEquipo':'sist4','Procesador':'i7','AnioCompra':2018,'ValorCompra':3200000,'Activo':'No'}            
+        ]
+       }                   
+   ] 
+}
