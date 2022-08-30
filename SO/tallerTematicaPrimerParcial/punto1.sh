4189fb545e6137414cd687549bfb12d6148f54c8
luismescobarf <luismescobarf@gmail.com>
2022-08-30 15:06:43 -0500

    Incorporación Sistemas Operativos en el repositorio personal UniLibre

---
+++ SO/tallerTematicaPrimerParcial/punto1.sh
@@ -0,0 +1,2 @@
+#1) Generar un archivo con el listado de procesos que está activos acualmente.
+ps -ef > procesosActivosActualmente.txt
---
+++ SO/tallerTematicaPrimerParcial/punto2.sh
@@ -0,0 +1,2 @@
+#2) ¿Cuántos pacientes había en el estudio de cáncer?
+grep -v provincia cancer_progresion.txt | wc -l
