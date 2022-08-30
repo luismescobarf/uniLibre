6297dd7e07c1dfc2909b81805c92a0898aeab57c
luismescobarf <luismescobarf@gmail.com>
2022-08-30 15:23:46 -0500

    Avence taller propuesto y reorganización de la solución

---
+++ SO/tallerTematicaPrimerParcial/loteSoluciones.sh
@@ -0,0 +1,44 @@
+#Activar y desactivar las líneas del lote que se quieran utilizar
+
+# #1) Generar un archivo con el listado de procesos que está activos acualmente.
+# ps -ef > procesosActivosActualmente.txt
+
+# #2) ¿Cuántos pacientes había en el estudio de cáncer?
+# grep -v provincia cancer_progresion.txt | wc -l
+
+# #3) ¿De cuántos pacientes no tenemos datos de progresión?
+# grep desconocido cancer_progresion.txt | wc -l 
+
+# 5) Convertir la separación de comas de la tabla de doble ciego a tabuladores.
+cat cancer_ciego.txt | tr ',' '\t' > cancer_ciego_tab.txt
+ 
+
+# 6) Unir la tabla de los resultados de la terapia con la del doble ciego.
+
+ 
+
+# 7) ¿Cómo les ha ido a los pacientes según el tipo de tratamiento? (Placebo está escrito con mayúsculas y minúsculas)
+
+ 
+
+# 8) Establecer una rutina que descargue el libro la odisea en formato de texto plano. Y obtener:
+# - Listado de líneas donde aparece la palabra 'which' y generar un archivo ocurrenciasWhich.txt con el resultado.
+# - Número de veces que aparece la palabra 'shall' y guardar el resultado en un fichero.
+
+ 
+
+# 9) Escribir un archicho .sh por lotes que descargue el archivo comprimido ubicado en:
+ 
+# https://github.com/luismescobarf/uniLibre/raw/main/paquete.tar.gz
+
+# - Generar un archivo con los enlaces simbólicos presentes en el directorio.
+# - ¿Cuántos directorios hay en el directorio descargado?
+# - Copiar todos los scripts de Python y generar una carpeta en el home del usuario actual llamada rutinasPython,
+#     dejando esos archivos con permisos de ejecución únicamente para el grupo.
+# - Copiar todos los archivos .txt a una carpeta en el home que se denomine BackUP_TXT con todos estos archivos comprimidos.
+
+ 
+
+# 10) Generar un script o un pipe o una mezcla de ambas, para dejar en un archivo el informe de cuántas
+# veces se ha utilizado el comando tar y en cuáles líneas.
+
--- SO/tallerTematicaPrimerParcial/punto2.sh
+++
@@ -1,2 +0,0 @@
-#2) ¿Cuántos pacientes había en el estudio de cáncer?
-grep -v provincia cancer_progresion.txt | wc -l
